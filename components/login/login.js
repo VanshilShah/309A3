578cc62e74f617d5a4befdb0b939a11703333033
TommyX12 <tommyx058@gmail.com>
2017-12-02 20:02:34 -0500

    added logout

--- components/login/login.js
+++ components/login/login.js
@@ -2,9 +2,17 @@ var login = (function(){
     var obj = {}
     
     obj.gotoFriendsIfLoggedIn = function () {
+        $('#login-text').text('Loading ...')
+        $('#fb-login-btn').addClass('hidden')
+        
         fbInterface.checkLoginState(function (response) {
             if (fbCurrentState && fbCurrentState.status == 'connected') {
+                $('#navbar-logout').removeClass('hidden')
                 navbar.showPage('friends')
+            }
+            else {
+                $('#login-text').text('Login with Facebook to share your schedule')
+                $('#fb-login-btn').removeClass('hidden')
             }
         })
     }
@@ -12,6 +20,7 @@ var login = (function(){
     obj.tryLogOut = function () {
         if (confirm("Are you sure you want to log out?")) {
             FB.logout(function (respose) {
+                $('#navbar-logout').addClass('hidden')
                 navbar.showPage('courses')
             })
         }
