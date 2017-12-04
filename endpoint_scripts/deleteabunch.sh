79996414d33e7a27f700b2705ac1f2fe5c2d6047
Everybody0523 <h.neukc@gmail.com>
2017-12-03 19:38:40 -0500

    more scripts

---
+++ endpoint_scripts/deleteabunch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+curl --request DELETE https://shareschedule.herokuapp.com/api/messages/0
+curl --request DELETE https://shareschedule.herokuapp.com/api/messages/1
+curl --request DELETE https://shareschedule.herokuapp.com/api/messages/2
+curl --request DELETE https://shareschedule.herokuapp.com/api/messages/3
+curl --request DELETE https://shareschedule.herokuapp.com/api/messages/4
+curl --request DELETE https://shareschedule.herokuapp.com/api/messages/5
--- testcurl.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-curl -H "Content-Type: application/json" -X POST -d '{"body":"cow"}' https://shareschedule.herokuapp.com/api/messages
-curl -X GET https://shareschedule.herokuapp.com/api/messages
-curl -X DELETE https://shareschedule.herokuapp.com/api/messages/0
