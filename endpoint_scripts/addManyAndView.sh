5e6df6f24c34be2c3f64ac43b0c75d6a0cf75eec
Everybody0523 <h.neukc@gmail.com>
2017-12-04 01:04:05 -0500

    meep

--- endpoint_scripts/addManyAndView.sh
+++ endpoint_scripts/addManyAndView.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-curl -H "Content-Type: application/json" -X POST -d '{"body":"cow"}' https://shareschedule.herokuapp.com/api/messages
-curl -H "Content-Type: application/json" -X POST -d '{"body":"chicken"}' https://shareschedule.herokuapp.com/api/messages
-curl -H "Content-Type: application/json" -X POST -d '{"body":"pig"}' https://shareschedule.herokuapp.com/api/messages
-curl -H "Content-Type: application/json" -X POST -d '{"body":"donkey"}' https://shareschedule.herokuapp.com/api/messages
+curl -H "Content-Type: application/json" -X POST -d '{"data":"cow"}' https://shareschedule.herokuapp.com/api/messages
+curl -H "Content-Type: application/json" -X POST -d '{"data":"chicken"}' https://shareschedule.herokuapp.com/api/messages
+curl -H "Content-Type: application/json" -X POST -d '{"data":"pig"}' https://shareschedule.herokuapp.com/api/messages
+curl -H "Content-Type: application/json" -X POST -d '{"data":"donkey"}' https://shareschedule.herokuapp.com/api/messages
 curl -X GET https://shareschedule.herokuapp.com/api/messages
 printf "\n"
